d487b7b253703a593d9eb8a62595ca3010ceb0e4
Teodora Chiosa <teodora.chiosa@gmail.com>
2025-11-19 19:37:37 +0200

    Fix prerendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,17 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import waveAllInOne from '@wave/wave-all-in-one.json';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'monitorizare/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const allReports = waveAllInOne;
+      return allReports.map((report) => ({id: report.slug}));
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Server,
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
